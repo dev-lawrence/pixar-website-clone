62927b694fc5d7e8f4190413e5989ce3df000817
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2023-04-18 08:03:04 +0100

    finished the featuers section

--- src/pages/Feature.jsx
+++ src/pages/Feature.jsx
@@ -1,6 +1,7 @@
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
+import { FeatureData } from '../components/FeatureData';
 
 const Feature = () => {
   return (
@@ -8,6 +9,19 @@ const Feature = () => {
       <Header />
       <section className="feature">
         <Hero text="feature films" />
+
+        <div className="container">
+          <div className="feature-movies">
+            {FeatureData.map((data, index) => {
+              return (
+                <div key={index} className="movie">
+                  <img src={data.src} alt="" />
+                  <h4>{data.title}</h4>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </section>
       <Footer />
     </>
